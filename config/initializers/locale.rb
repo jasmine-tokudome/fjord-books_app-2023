9e171897be95a73bff33f236bb7f7527e9baf9ef
Jasmine <orange_blossom_1684mk@yahoo.co.jp>
2023-08-10 14:34:28 +0900

    UIを見やすく修正

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,7 +2,7 @@
 I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb, yml}')]
 
 # アプリケーションでの利用を許可するロケールのリストを渡す
-I18n.available_locales = [:en, :ja]
+I18n.available_locales = [:ja, :en]
 
 # ロケールを:en以外に変更する
 I18n.default_locale = :ja
