323dcb5055b710ae079a77b388d8b134929b6818
Jasmine <orange_blossom_1684mk@yahoo.co.jp>
2023-09-03 18:25:11 +0900

    日報の編集後、言及機能が編集後の内容に即したものとなるよう修正。

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -36,6 +36,14 @@ class ReportsController < ApplicationController
 
   def update
     if @report.update(report_params)
+      matching_mentions = Mention.where(mentioning_report_id: @report.id)
+      Mention.destroy(matching_mentions.ids)
+      extract_urls = params[:report][:content].scan(/http:\/\/localhost\:3000\/reports\/(\d+)/).uniq.flatten
+      if extract_urls.any?
+        extract_urls.each do |extract_url|
+          Mention.create(mentioning_report_id: @report.id, mentioned_report_id: extract_url)
+        end
+      end
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
@@ -43,6 +51,8 @@ class ReportsController < ApplicationController
   end
 
   def destroy
+    report_id = @report.id
+    Mention.find_by(mentioned_report_id: report_id).destroy
     @report.destroy
 
     redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
