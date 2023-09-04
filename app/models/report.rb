ab63cc430c7c59669c983ccb35682441897b9224
Jasmine <orange_blossom_1684mk@yahoo.co.jp>
2023-09-04 17:42:25 +0900

    不具合に気づいたためモデルの関連付け(inverse_of)を修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,9 +3,9 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-  has_many :mentioning, class_name: "Mention", foreign_key: :mentioning_report_id, dependent: :destroy, inverse_of: :mentioning_report
+  has_many :mentioning, class_name: 'Mention', foreign_key: :mentioning_report_id, dependent: :destroy, inverse_of: :mentioning_report
   # has_many :mentioning_reports, through: :mentioning
-  has_many :mentioned, class_name: "Mention", foreign_key: :mentioned_report_id, dependent: :destroy, inverse_of: :mentioned_report
+  has_many :mentioned, class_name: 'Mention', foreign_key: :mentioned_report_id, dependent: :destroy, inverse_of: :mentioned_report
   # has_many :mentioned_reports, through: :mentioned
 
   validates :title, presence: true
