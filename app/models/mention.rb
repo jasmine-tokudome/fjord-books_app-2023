972bf50154216367531e525ee4c90e61e63a6b42
Jasmine <orange_blossom_1684mk@yahoo.co.jp>
2023-09-04 16:47:41 +0900

    inverse_ofの記述漏れがあったので追記

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
+
 class Mention < ApplicationRecord
-  belongs_to :mentioning_report, class_name: 'Report'
-  belongs_to :mentioned_report, class_name: 'Report'
+  belongs_to :mentioning_report, class_name: 'Report', inverse_of: :reports
+  belongs_to :mentioned_report, class_name: 'Report', inverse_of: :reports
 end
