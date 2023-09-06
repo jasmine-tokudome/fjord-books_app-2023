0ee9b8308373fda3c7c003f9fa182cbb97423a6c
Jasmine <orange_blossom_1684mk@yahoo.co.jp>
2023-09-06 11:53:55 +0900

    report,bookの両方に対応できるようviewsでのデータの呼び出しをcommentableで行うよう変更

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -13,8 +13,6 @@ class ReportsController < ApplicationController
   # GET /reports/1 or /reports/1.json
   def show
     @report = Report.find(params[:id])
-    @comment = Comment.new
-    @comments = @report.comments
   end
 
   # GET /reports/new
